eb477eb87430ee74390da97e64b247878cae9de2
Thalyson Luiz <thalysonluiz@gmail.com>
2023-08-10 22:19:17 -0300

    style tabBar

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Feather } from '@expo/vector-icons';
 import { Home } from '@screens/Home';
 import { Profile } from '@screens/Profile';
 import { Search } from '@screens/Search';
@@ -7,10 +8,44 @@ const Tab = createBottomTabNavigator();
 
 export function AppRoutes() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Search" component={Search} />
-      <Tab.Screen name="Profile" component={Profile} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarHideOnKeyboard: true,
+        tabBarStyle: {
+          borderTopWidth: 0,
+          backgroundColor: '#202225'
+        },
+        tabBarActiveTintColor: '#FFFFFF',
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="home" color={color} size={size} />
+          )
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="search" color={color} size={size} />
+          )
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="user" color={color} size={size} />
+          )
+        }}
+      />
     </Tab.Navigator>
   )
 }
