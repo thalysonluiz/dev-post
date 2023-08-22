8f1ab3d89fb1f44e30cd4870e416ff8656a651fa
Thalyson Luiz <thalysonluiz@gmail.com>
2023-08-22 15:10:03 -0300

    profile screen

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import { useAuth } from '@hooks/useAuth';
-import { Container } from './styles';
-import { Button } from 'react-native';
+import { Header } from '@components/Header';
+import { Avatar, Container, Email, Name, UploadButton, UploadText, ButtonText, Button } from './styles';
 
 export function Profile() {
-  const { signOut } = useAuth();
+  const [url, setUrl] = useState(null);
+  const { signOut, user } = useAuth();
 
   function handleSignOut() {
     signOut();
@@ -11,7 +13,33 @@ export function Profile() {
 
   return (
     <Container>
-      <Button title='Sair' onPress={handleSignOut} />
+      <Header />
+      {
+        url ? (
+          <UploadButton>
+            <UploadText>+</UploadText>
+            <Avatar
+              source={{ uri: url }}
+            />
+          </UploadButton>
+        ) : (
+          <UploadButton>
+            <UploadText>+</UploadText>
+            <Avatar
+              source={require('@assets/avatar.png')}
+            />
+          </UploadButton>
+        )
+      }
+      <Name numberOfLines={1}>{user.name}</Name>
+      <Email numberOfLines={1}>{user.email}</Email>
+
+      <Button bg="#428cfd" onPress={handleSignOut} >
+        <ButtonText color="#fff">Atualizar Perfil</ButtonText>
+      </Button>
+      <Button bg="#f1f1f1" onPress={handleSignOut} >
+        <ButtonText color="#3b3b3b">Sair</ButtonText>
+      </Button>
     </Container>
   );
 }
--- src/screens/Profile/styles.ts
+++ src/screens/Profile/styles.ts
@@ -1,5 +1,65 @@
-import styled from 'styled-components/native';
+import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
+  align-items: center;
+  background-color: #353840;
+`;
+
+export const UploadButton = styled.TouchableOpacity`
+  margin-top: 20%;
+  background-color: #fff;
+  width: 165px;
+  height: 165px;
+  border-radius: 90px;
+  justify-content: center;
+  align-items: center;
+  z-index: 5;
+`;
+
+export const UploadText = styled.Text`
+  z-index: 9;
+  position: absolute;
+  font-size: 55px;
+  color: #e52246;
+  opacity: 0.4;
+`;
+
+export const Avatar = styled.Image`
+  width: 160px;
+  height: 160px;
+  border-radius: 80px;
+  opacity: 0.9;
+`;
+
+export const Name = styled.Text`
+  margin-top: 20px;
+  margin-left: 20px;
+  margin-right: 20px;
+  font-size: 28px;
+  color: #fff;
+  font-weight: bold;
+`;
+
+export const Email = styled.Text`
+  margin: 10px 20px;
+  font-size: 20px;
+  color: #ddd;
+  font-style: italic;
+`;
+
+export const Button = styled.TouchableOpacity`
+  margin-top: 12px;
+  align-items: center;
+  justify-content: center;
+  background-color: ${(props) => props.bg};
+  width: 80%;
+  height: 45px;
+  border-radius: 5px;
+`;
+
+export const ButtonText = styled.Text`
+  color: ${(props) => props.color};
+  font-size: 20px;
+  font-style: italic;
 `;
